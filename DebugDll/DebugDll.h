819771b451ad614bd36ba3c6c88a09b45f0df8ce
M-Yuan <417684240@qq.com>
2016-09-02 15:40:30 +0800

    Update DebugDll.h

--- DebugDll/DebugDll.h
+++ DebugDll/DebugDll.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#pragma comment(lib, "../Debug/CreateDll.lib")
 #pragma comment(lib, "../Release/CreateDll.lib")
 extern "C" _declspec(dllimport) int __stdcall test01(int a, int b, int c);
 extern "C" _declspec(dllimport) int __stdcall test02(int a, int b);
